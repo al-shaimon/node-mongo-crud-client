ef27bbb96a69e4ee9c1c1d5dae7cef84ce3cf364
al-shaimon <alshaimon968@gmail.com>
2022-11-11 00:27:54 +0600

    65-7 Load Single Item By Id And Update User Info

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Home = () => {
   const users = useLoaderData();
@@ -30,7 +30,11 @@ const Home = () => {
       <div>
         {displayUsers.map((user) => (
           <p key={user._id}>
-            {user.name} {user.email} <button onClick={() => handleDelete(user)}>X</button>
+            {user.name} {user.email}
+            <Link to={`/update/${user._id}`}>
+              <button>update</button>
+            </Link>
+            <button onClick={() => handleDelete(user)}>X</button>
           </p>
         ))}
       </div>
